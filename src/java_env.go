23b5c752648e3f369e6f47e0f756b09721a01ea5
wubingheng <wubinghengajw@outlook.com>
2019-07-03 11:49:26 +0800

    test "java version" out string successfully.

--- src/java_env.go
+++ src/java_env.go
@@ -4,15 +4,13 @@ import (
 	"bytes"
 	"fmt"
 	"os/exec"
-	"syscall"
 )
 
 func JdkDownload() {
 
 	//函数返回一个*Cmd，用于使用给出的参数执行name指定的程序
-	cmd := exec.Command("/bin/bash", "-c", "java -version")
-	//cmd := exec.Command("java", "-version")
-	fmt.Println(cmd)
+	//cmd := exec.Command("/bin/bash", "-c", "java -version")
+	cmd := exec.Command("java", "-version")
 	var out bytes.Buffer
 	var stderr bytes.Buffer
 	cmd.Stdout = &out
@@ -23,9 +21,23 @@ func JdkDownload() {
 	} else {
 		fmt.Println(out.String())
 	}
-	fmt.Println(cmd.ProcessState.Sys() == syscall.WaitStatus(0))
-	fmt.Println(cmd)
-	fmt.Println(cmd)
+	//fmt.Println(cmd.ProcessState.Sys() == syscall.WaitStatus(0))
+	result := fmt.Sprintln(cmd)
+	fmt.Println(result)
+	//fmt.Println(strings.Split(result, "  <nil>  ")[1])
+
+	fmt.Println("Path: " + cmd.Path)
+	fmt.Print("Args: ")
+	fmt.Println(cmd.Args)
+	fmt.Print("Env: ")
+	fmt.Println(cmd.Env)
+	fmt.Println("Dir: " + cmd.Dir)
+	fmt.Print("SysProcAttr: ")
+	fmt.Println(cmd.SysProcAttr)
+	fmt.Print("ExtraFiles: ")
+	fmt.Println(cmd.ExtraFiles)
+	fmt.Println(cmd.Process)
+	fmt.Println(stderr.String())
 }
 
 func main()  {
